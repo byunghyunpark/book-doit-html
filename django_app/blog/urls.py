711cabae9263d4ac0975947847b23c2ca348e35e
byunghyunpark <openmind8735@gmail.com>
2017-01-10 12:15:10 +0900

    Add part2-2 practice

--- django_app/blog/urls.py
+++ django_app/blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.blog, name='blog'),
+    url(r'^part2/layout1/', views.part2_layout1, name='layout1'),
 ]
--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def blog(request):
     context = {}
     return render(request, 'blog/index.html', context)
+
+
+def part2_layout1(request):
+    context = {}
+    return render(request, 'part2/layout1.html', context)
